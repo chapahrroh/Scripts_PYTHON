a0940eb33c8b3c72707edbb64dc252125475c13a
chapahrroh <chapahrroh@gmail.com>
2021-10-26 21:50:15 -0300

    avances en ping.py

--- ping.py
+++ ping.py
@@ -1,15 +1,29 @@
 #Autor: Eduardo Chaparro
 
-from requests import get, exceptions
+import ping3
+ping3.EXCEPTIONS = True
+
 
 def pingCheck():
-    print("ingrese direccion:", end="")
-    direccion = input()
+    #identificacion de antenna
+    print("ingrese antenna:", end="")
+    prefijo = '10.192.'
+    antenna = input()
+    subfijo_ACU = '.21'
+
+    #identificacion antenna
+    if antenna>='41' and antenna<='65':
+        prefijoAntenna = 'DA'
+
+    print (prefijo+antenna+subfijo_ACU)
+    #prueba de coneccion
     try:
-        get("http://"+direccion, timeout=3)
-        print('conectado')
-    except exceptions.ConnectionError:
-        print( "\033[1;31;42m"+"no conectado")
+        ping3.ping(prefijo+antenna+subfijo_ACU)
+        print('ACU conectada')
+    except ping3.errors.HostUnknown:
+        print( "\033[1;31;42m"+"error direccion antenna")
+    except ping3.errors.PingError:
+        print('ACU '+prefijoAntenna+antenna+' sin conexion')
 
 pingCheck()
 
